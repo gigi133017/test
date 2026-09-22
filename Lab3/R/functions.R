2a8e1abe323bde5b89b75a9555ed579a34793356
gigi133017 <giulio.collazuol@gmail.com>
2026-09-22 18:30:50 +0200

    tests, data, documentation

--- Lab3/R/functions.R
+++ Lab3/R/functions.R
@@ -2,10 +2,14 @@
 #liu-id
 #' euclidean
 #'
-#'This function implements euclidean
+#'This function implements euclidean algorithm to calculate the MCD of two given numbers (a, b).
+#'This function iteratively calculates the remainder of the whole division between the previous two remainders.
+#'The output is the last non-zero remainder
 #'
 #' @param a integer scalar
 #' @param b integer scalar
+#' @return  integer scalar
+#' @references https://en.wikipedia.org/wiki/Euclidean_algorithm
 #' @export
 euclidean <- function(a, b){
     stopifnot(is.numeric(a), is.numeric(b), a%%1 == 0, b%%1 == 0, length(a)==1, length(b)==1)
@@ -25,18 +29,21 @@ euclidean <- function(a, b){
         smaller <- rest
     }
 
-    return(smaller)
+    return(abs(smaller))
 }
 
-#' dijkstra
+#' Dijkstra
 #'
-#'This function implements dijkstra
+#'This function implements Dijkstra algorithm finding the shortest path between two nodes in a graph.
+#'For each node in the graph, the function updates the distances vector if it finds a new shortest path to its connected nodes.
 #'
-#' @param graph data_frame with three colums
-#' @param init_node node from whitch we want to calculate the distancies
+#' @param graph data_frame with three columns v1, v2, w. The three columns describe the nodes of an edge and its weight
+#' @param init_node node from which we want to calculate the distances from
+#' @return vector of dimension of the number of nodes in the graph
+#' @references https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
 #' @export
 dijkstra <- function(graph, init_node){
-    stopifnot(is.data.frame(graph), names(graph)==c("v1","v2","w"), init_node%%1==0, is.numeric(init_node),
+    stopifnot(is.data.frame(graph), identical(names(graph), c("v1","v2","w")), init_node%%1==0, is.numeric(init_node),
               length(init_node)==1, init_node %in% append(wiki_graph[[1]], wiki_graph[[2]]) )
 
 
@@ -69,7 +76,16 @@ dijkstra <- function(graph, init_node){
     return(weight_vector)
 }
 
-
-# wiki_graph <- data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
-#                          v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
-#                          w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
+#'Data
+#'
+#'Example graph with six nodes
+#'
+#'@format ## dataframe six nodes and eighteen edges and the distancies between them
+#'\describe{
+#'  \item{v1, v2}{nodes of the edges}
+#'  \item{w}{weights of the edges}
+#'}
+#'@source https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+wiki_graph <- data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
+                         v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
+                         w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
